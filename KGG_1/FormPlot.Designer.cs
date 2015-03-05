9efe27951bc0e10044ca48838b6456ab5a61ea29
nest-aka-swan <swan.nikita@gmail.com>
2015-03-05 16:33:39 +0500

    add tooltip?

--- KGG_1/FormPlot.Designer.cs
+++ KGG_1/FormPlot.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             this.groupBoxParameters = new System.Windows.Forms.GroupBox();
-            this.labelA = new System.Windows.Forms.Label();
-            this.labelB = new System.Windows.Forms.Label();
-            this.textBoxA = new System.Windows.Forms.TextBox();
-            this.textBoxB = new System.Windows.Forms.TextBox();
-            this.labelAlpha = new System.Windows.Forms.Label();
-            this.textBoxAlpha = new System.Windows.Forms.TextBox();
-            this.labelBeta = new System.Windows.Forms.Label();
-            this.textBoxBeta = new System.Windows.Forms.TextBox();
-            this.buttonDrawPlot = new System.Windows.Forms.Button();
-            this.labelC = new System.Windows.Forms.Label();
             this.textBoxC = new System.Windows.Forms.TextBox();
+            this.labelC = new System.Windows.Forms.Label();
+            this.buttonDrawPlot = new System.Windows.Forms.Button();
+            this.textBoxBeta = new System.Windows.Forms.TextBox();
+            this.labelBeta = new System.Windows.Forms.Label();
+            this.textBoxAlpha = new System.Windows.Forms.TextBox();
+            this.labelAlpha = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.labelB = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
             this.groupBoxPlot = new System.Windows.Forms.GroupBox();
+            this.pictureBoxPlot = new System.Windows.Forms.PictureBox();
             this.groupBoxTask = new System.Windows.Forms.GroupBox();
             this.listBoxTask = new System.Windows.Forms.ListBox();
-            this.pictureBoxPlot = new System.Windows.Forms.PictureBox();
             this.customizedToolTipDrawPlot = new KGG_1.CustomizedToolTip();
             this.groupBoxParameters.SuspendLayout();
             this.groupBoxPlot.SuspendLayout();
-            this.groupBoxTask.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlot)).BeginInit();
+            this.groupBoxTask.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxParameters
@@ -74,79 +74,12 @@
             this.groupBoxParameters.TabStop = false;
             this.groupBoxParameters.Text = "Введите параметры";
             // 
-            // labelA
+            // textBoxC
             // 
-            this.labelA.AutoSize = true;
-            this.labelA.Location = new System.Drawing.Point(7, 20);
-            this.labelA.Name = "labelA";
-            this.labelA.Size = new System.Drawing.Size(17, 13);
-            this.labelA.TabIndex = 0;
-            this.labelA.Text = "A:";
-            // 
-            // labelB
-            // 
-            this.labelB.AutoSize = true;
-            this.labelB.Location = new System.Drawing.Point(86, 20);
-            this.labelB.Name = "labelB";
-            this.labelB.Size = new System.Drawing.Size(17, 13);
-            this.labelB.TabIndex = 0;
-            this.labelB.Text = "B:";
-            // 
-            // textBoxA
-            // 
-            this.textBoxA.Location = new System.Drawing.Point(30, 17);
-            this.textBoxA.Name = "textBoxA";
-            this.textBoxA.Size = new System.Drawing.Size(50, 20);
-            this.textBoxA.TabIndex = 0;
-            // 
-            // textBoxB
-            // 
-            this.textBoxB.Location = new System.Drawing.Point(109, 17);
-            this.textBoxB.Name = "textBoxB";
-            this.textBoxB.Size = new System.Drawing.Size(50, 20);
-            this.textBoxB.TabIndex = 1;
-            // 
-            // labelAlpha
-            // 
-            this.labelAlpha.AutoSize = true;
-            this.labelAlpha.Location = new System.Drawing.Point(244, 20);
-            this.labelAlpha.Name = "labelAlpha";
-            this.labelAlpha.Size = new System.Drawing.Size(17, 13);
-            this.labelAlpha.TabIndex = 0;
-            this.labelAlpha.Text = "α:";
-            // 
-            // textBoxAlpha
-            // 
-            this.textBoxAlpha.Location = new System.Drawing.Point(267, 17);
-            this.textBoxAlpha.Name = "textBoxAlpha";
-            this.textBoxAlpha.Size = new System.Drawing.Size(50, 20);
-            this.textBoxAlpha.TabIndex = 3;
-            // 
-            // labelBeta
-            // 
-            this.labelBeta.AutoSize = true;
-            this.labelBeta.Location = new System.Drawing.Point(323, 20);
-            this.labelBeta.Name = "labelBeta";
-            this.labelBeta.Size = new System.Drawing.Size(16, 13);
-            this.labelBeta.TabIndex = 0;
-            this.labelBeta.Text = "β:";
-            // 
-            // textBoxBeta
-            // 
-            this.textBoxBeta.Location = new System.Drawing.Point(345, 17);
-            this.textBoxBeta.Name = "textBoxBeta";
-            this.textBoxBeta.Size = new System.Drawing.Size(50, 20);
-            this.textBoxBeta.TabIndex = 4;
-            // 
-            // buttonDrawPlot
-            // 
-            this.buttonDrawPlot.Location = new System.Drawing.Point(401, 15);
-            this.buttonDrawPlot.Name = "buttonDrawPlot";
-            this.buttonDrawPlot.Size = new System.Drawing.Size(158, 23);
-            this.buttonDrawPlot.TabIndex = 5;
-            this.buttonDrawPlot.Text = "Построить график";
-            this.customizedToolTipDrawPlot.SetToolTip(this.buttonDrawPlot, "Функция");
-            this.buttonDrawPlot.UseVisualStyleBackColor = true;
+            this.textBoxC.Location = new System.Drawing.Point(188, 17);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(50, 20);
+            this.textBoxC.TabIndex = 3;
             // 
             // labelC
             // 
@@ -157,12 +90,80 @@
             this.labelC.TabIndex = 0;
             this.labelC.Text = "C:";
             // 
-            // textBoxC
+            // buttonDrawPlot
             // 
-            this.textBoxC.Location = new System.Drawing.Point(188, 17);
-            this.textBoxC.Name = "textBoxC";
-            this.textBoxC.Size = new System.Drawing.Size(50, 20);
-            this.textBoxC.TabIndex = 2;
+            this.buttonDrawPlot.Location = new System.Drawing.Point(401, 15);
+            this.buttonDrawPlot.Name = "buttonDrawPlot";
+            this.buttonDrawPlot.Size = new System.Drawing.Size(158, 23);
+            this.buttonDrawPlot.TabIndex = 6;
+            this.buttonDrawPlot.Text = "Построить график";
+            this.customizedToolTipDrawPlot.SetToolTip(this.buttonDrawPlot, "Функция");
+            this.buttonDrawPlot.UseVisualStyleBackColor = true;
+            this.buttonDrawPlot.Click += new System.EventHandler(this.buttonDrawPlot_Click);
+            // 
+            // textBoxBeta
+            // 
+            this.textBoxBeta.Location = new System.Drawing.Point(345, 17);
+            this.textBoxBeta.Name = "textBoxBeta";
+            this.textBoxBeta.Size = new System.Drawing.Size(50, 20);
+            this.textBoxBeta.TabIndex = 5;
+            // 
+            // labelBeta
+            // 
+            this.labelBeta.AutoSize = true;
+            this.labelBeta.Location = new System.Drawing.Point(323, 20);
+            this.labelBeta.Name = "labelBeta";
+            this.labelBeta.Size = new System.Drawing.Size(16, 13);
+            this.labelBeta.TabIndex = 0;
+            this.labelBeta.Text = "β:";
+            // 
+            // textBoxAlpha
+            // 
+            this.textBoxAlpha.Location = new System.Drawing.Point(267, 17);
+            this.textBoxAlpha.Name = "textBoxAlpha";
+            this.textBoxAlpha.Size = new System.Drawing.Size(50, 20);
+            this.textBoxAlpha.TabIndex = 4;
+            // 
+            // labelAlpha
+            // 
+            this.labelAlpha.AutoSize = true;
+            this.labelAlpha.Location = new System.Drawing.Point(244, 20);
+            this.labelAlpha.Name = "labelAlpha";
+            this.labelAlpha.Size = new System.Drawing.Size(17, 13);
+            this.labelAlpha.TabIndex = 0;
+            this.labelAlpha.Text = "α:";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(109, 17);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(50, 20);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(30, 17);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(50, 20);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(86, 20);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(17, 13);
+            this.labelB.TabIndex = 0;
+            this.labelB.Text = "B:";
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(7, 20);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(17, 13);
+            this.labelA.TabIndex = 0;
+            this.labelA.Text = "A:";
             // 
             // groupBoxPlot
             // 
@@ -178,6 +179,18 @@
             this.groupBoxPlot.TabStop = false;
             this.groupBoxPlot.Text = "График";
             // 
+            // pictureBoxPlot
+            // 
+            this.pictureBoxPlot.BackColor = System.Drawing.Color.White;
+            this.pictureBoxPlot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPlot.Location = new System.Drawing.Point(3, 16);
+            this.pictureBoxPlot.Name = "pictureBoxPlot";
+            this.pictureBoxPlot.Size = new System.Drawing.Size(556, 464);
+            this.pictureBoxPlot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPlot.TabIndex = 0;
+            this.pictureBoxPlot.TabStop = false;
+            this.pictureBoxPlot.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxPlot_Paint);
+            // 
             // groupBoxTask
             // 
             this.groupBoxTask.Controls.Add(this.listBoxTask);
@@ -191,22 +204,16 @@
             // listBoxTask
             // 
             this.listBoxTask.FormattingEnabled = true;
+            this.listBoxTask.Items.AddRange(new object[] {
+            "График функции",
+            "Алгоритм Брезенхема",
+            "Многоугольники",
+            "График поверхности",
+            "Удаление невидимых линий"});
             this.listBoxTask.Location = new System.Drawing.Point(6, 19);
             this.listBoxTask.Name = "listBoxTask";
             this.listBoxTask.Size = new System.Drawing.Size(182, 524);
             this.listBoxTask.TabIndex = 0;
-            // 
-            // pictureBoxPlot
-            // 
-            this.pictureBoxPlot.BackColor = System.Drawing.Color.White;
-            this.pictureBoxPlot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxPlot.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxPlot.Location = new System.Drawing.Point(3, 16);
-            this.pictureBoxPlot.Name = "pictureBoxPlot";
-            this.pictureBoxPlot.Size = new System.Drawing.Size(556, 464);
-            this.pictureBoxPlot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBoxPlot.TabIndex = 0;
-            this.pictureBoxPlot.TabStop = false;
             // 
             // customizedToolTipDrawPlot
             // 
@@ -232,8 +239,8 @@
             this.groupBoxParameters.ResumeLayout(false);
             this.groupBoxParameters.PerformLayout();
             this.groupBoxPlot.ResumeLayout(false);
-            this.groupBoxTask.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlot)).EndInit();
+            this.groupBoxTask.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- KGG_1/FormPlot.cs
+++ KGG_1/FormPlot.cs
@@ -1,6 +1,5 @@
 ﻿using KGG_1.Properties;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,31 +8,73 @@ namespace KGG_1
 {
     public partial class FormPlot : Form
     {
-        List<string> _taskItems = new List<string>();
-
+        private int A, B, C, Alpha, Beta;
         public FormPlot()
         {
             InitializeComponent();
 
-            // Панель выбора задач
-            _taskItems.Add("График функции");
-            _taskItems.Add("Алгоритм Брезенхема");
-            _taskItems.Add("Многоугольники");
-            _taskItems.Add("График поверхности");
-            _taskItems.Add("Удаление невидимых линий");
-
-            listBoxTask.DataSource = _taskItems;
-
             // Тултип с функцией
             buttonDrawPlot.Tag = Resources.Task1Function;
-            //fucking git
 
             // Значения по умолчанию
             textBoxA.Text = "1";
-            textBoxB.Text = "1";
-            textBoxC.Text = "1";
-            textBoxAlpha.Text = "-10";
-            textBoxBeta.Text = "10";
+            textBoxB.Text = "0";
+            textBoxC.Text = "0";
+            textBoxAlpha.Text = "-5";
+            textBoxBeta.Text = "5";
+
+            InitCoefficients();
         }
+
+        private void buttonDrawPlot_Click(object sender, EventArgs e)
+        {
+            InitCoefficients();
+            pictureBoxPlot.Invalidate();
+        }
+
+        private void pictureBoxPlot_Paint(object sender, PaintEventArgs e)
+        {
+            int maxX = pictureBoxPlot.Width;
+            int maxY = pictureBoxPlot.Height;
+            int yy, xxPrev = -1, yyPrev = maxY / 2;
+            float x, y;
+            //нарисовать оси, вычислив центр
+            for(int xx = 0; xx < maxX; ++xx)
+            {
+                x = Alpha + (float)(xx * (Beta - Alpha))/(float)maxX;
+                y = A * x / ((B + x) * (C - x) * (C - x));
+                yy = (int)(((y - Beta) * maxY)/((float)(Alpha - Beta)));
+                e.Graphics.DrawLine(Pens.Red, xxPrev, yyPrev, xx, yy);
+                xxPrev = xx;
+                yyPrev = yy;
+            }
+        }
+
+        private void InitCoefficients()
+        {
+            A = Int32.Parse(textBoxA.Text);
+            B = Int32.Parse(textBoxB.Text);
+            C = Int32.Parse(textBoxC.Text);
+            Alpha = Int32.Parse(textBoxAlpha.Text);
+            Beta = Int32.Parse(textBoxBeta.Text);
+        }
+
+        // двойной клик на пункт
+        //void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        //{
+        //    int index = this.listBox1.IndexFromPoint(e.Location);
+        //    if (index != System.Windows.Forms.ListBox.NoMatches)
+        //    {
+        //        MessageBox.Show(index.ToString());
+        //    }
+        //}
+
+        //как скрывать элементы
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    groupbox1.Visible = true;
+        //    groupbox2.Visible = !groupbox1.Visible;
+        //    groupbox3..... = !groupbox1.Visible; //etc
+        //}
     }
 }
